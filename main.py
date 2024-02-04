ffb988680b54c17b119c947834b9c42ff2c30a4f
lmcandido <123896641+lmcandido@users.noreply.github.com>
2024-02-03 23:17:11 -0800

    changes

---
+++ main.py
@@ -0,0 +1,10 @@
+age = int(input("How old are you?"))
+
+if age >= 18:
+    print("You are an adult")
+elif age == 100:
+    print("You are very old")
+elif age <0:
+    print("You haven't been born yet!")
+else:
+    print("You are a child")
